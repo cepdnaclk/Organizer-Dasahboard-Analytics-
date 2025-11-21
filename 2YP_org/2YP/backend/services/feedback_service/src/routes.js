d1deb81258ec355ae7e7441cd36b2fe47229eb78
Oshada Shashinidu <e21410@eng.pdn.ac.lk>
2025-11-21 20:17:12 +0530

    Frontend part added

--- 2YP_org/2YP/backend/services/feedback_service/src/routes.js
+++
@@ -1,9 +0,0 @@
-// services/feedback_service/src/routes.js
-const express = require('express');
-const { fetchFeedback } = require('./feedbackController');
-
-const router = express.Router();
-
-router.get('/feedback/details', fetchFeedback);
-
-module.exports = router;
